37dc92b3a44158f512a00fcc5020ace1beff2c7e
qqinxl <qqinxl@gmail.com>
2012-10-08 14:52:22 +0900

    devise user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def is_homepage?
+		(controller.action_name =~ /index/ and controller.controller_name =~ /home/)
+	end
+	def is_active_class(action_name)
+		"active" if (params[:controller] == action_name)
+		""
+	end
 end
