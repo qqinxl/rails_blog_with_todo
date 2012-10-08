c9e8abc23562d5ec758cb948e217feec45d74e20
qqinxl <qqinxl@gmail.com>
2012-10-08 17:10:46 +0900

    devise:modified

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,10 @@ module ApplicationHelper
 		(controller.action_name =~ /index/ and controller.controller_name =~ /home/)
 	end
 	def is_active_class(action_name)
-		"active" if (params[:controller] == action_name)
-		""
+		if (params[:controller] == action_name)
+			"active"
+		else
+			"" 
+		end
 	end
 end
